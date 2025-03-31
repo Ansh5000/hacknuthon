75d4ffea0d8f77275b48146ac990e020ad8a3a97
Ansh5000 <sonianshcsit@gmail.com>
2025-03-31 13:09:40 +0530

    new.py

--- new.py
+++ new.py
@@ -6,7 +6,9 @@ df = pd.DataFrame(df)
 # print(df)
 d={}
 x = 0
-l50=l25=l0=[]
+l50=[]
+l0=[]
+l25=[]
 df1=df
 for i in df['Records']:
     # print(i)
@@ -21,25 +23,50 @@ print(l)
 cnt=0
 cnttt=b=0
 d=0
+d_1={'410':[],'435':[],'460':[],'485':[],'510':[],'535':[],'560':[],'585':[],'610':[],'645':[],'680':[],'705':[],'730':[],'760':[],'810':[],'860':[],'900':[],'940':[],'Capacitity Moist':[],'Temp':[],'Moist':[],'EC (u/10 gram)':[],'Ph':[],'Nitro (mg/10 g)':[],'Posh Nitro (mg/10 g)':[],'Pota Nitro (mg/10 g)':[]}
 row_values=row_values_25=row_values_50=list()
 d_0={'Records':[],'410':[],'435':[],'460':[],'485':[],'510':[],'535':[],'560':[],'585':[],'610':[],'645':[],'680':[],'705':[],'730':[],'760':[],'810':[],'860':[],'900':[],'940':[],'Capacitity Moist':[],'Temp':[],'Moist':[],'EC (u/10 gram)':[],'Ph':[],'Nitro (mg/10 g)':[],'Posh Nitro (mg/10 g)':[],'Pota Nitro (mg/10 g)':[]}
+column_names=list(d_0.keys())
 for i in l:
-    if d==1:
-        break 
+    # if d==1:
+    #     break 
     # print(i)
     subset=df1.iloc[cnt:(i+cnt)]
     # print(subset)
     cnt+=i
-    d+=1
+    # d+=1
     for index,row in subset.iterrows():
-        if '_0ml' in row.values.tolist()[0]:
+        record_value = row['Records']
+        if '_0ml' in record_value:
             row_values_0 = row.values.tolist()
-            cnttt+=1
-        # print(row_values_0)
-        l0.append(row_values_0)
-        if '_25ml' in row.values.tolist()[0]:
+            # cnttt+=1
+            # print(row_values_0)
+            l0.append(row_values_0)
+        elif '_25ml' in record_value:
             row_values_25=row.values.tolist()
-        l25.append(row_values_25)   
-        if '_50ml' in row.values.tolist()[0]:
+            l25.append(row_values_25)
+            # print(row_values_25)
+        elif '_50ml' in record_value:
             row_values_50=row.values.tolist()
-        l50.append(row_values_50)   
+            l50.append(row_values_50)
+    df2=pd.DataFrame(l0,columns=column_names)
+    df2=df2.drop(columns=['Records'])
+    mean_values=df2.mean()
+    # df3=pd.DataFrame(df2)
+    for key in mean_values.index:
+        d_1[key].append(mean_values[key])
+    df3=pd.DataFrame(l25,columns=column_names)
+    df3=df3.drop(columns=['Records'])
+    mean_values1=df3.mean()
+    # df3=pd.DataFrame(df2)
+    for key in mean_values1.index:
+        d_1[key].append(mean_values1[key])
+    df4=pd.DataFrame(l0,columns=column_names)
+    df4=df4.drop(columns=['Records'])
+    mean_values2=df4.mean()
+    # df3=pd.DataFrame(df2)
+    for key in mean_values2.index:
+        d_1[key].append(mean_values2[key])
+df6=pd.DataFrame(d_1)
+# print(len(df6))
+print(df6.corr())
